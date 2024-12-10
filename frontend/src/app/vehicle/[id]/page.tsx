bc620ff6855cbd40f2b5560bcc1dff89d0570ffc
Ceulain <ceul1barth@hotmail.fr>
2024-12-10 10:38:42 +0100

    feat(frontend): display vehicle properties

--- frontend/src/app/vehicle/[id]/page.tsx
+++ frontend/src/app/vehicle/[id]/page.tsx
@@ -1,8 +1,21 @@
+import { fetchVehicle } from "@/app/lib/data";
+import { Box, Typography } from "@mui/material";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  return <div>My Vehicule: {id}</div>;
+  const vehicle = await fetchVehicle(id);
+
+  return (
+    <Box margin="16px">
+      {Object.entries(vehicle).map(([key, value]) => (
+        <Typography key={key} variant="body2" sx={{ color: "text.secondary" }}>
+          {`${key}: ${value}`}
+        </Typography>
+      ))}
+    </Box>
+  );
 }
