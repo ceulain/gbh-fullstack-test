bbcb442bda59561ef91b23ad46f49cbf9e4c06e1
Ceulain <ceul1barth@hotmail.fr>
2024-12-10 10:37:17 +0100

    feat(frontend): add fetch vehicle details

--- frontend/src/app/lib/data.ts
+++ frontend/src/app/lib/data.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Vehicle } from "../components/content/vehicle/types";
 import { Filter } from "../components/filter/types";
 
@@ -7,11 +8,24 @@ export const fetchVehicles = async (searchParams = {}) => {
     `http://localhost:3000/api/vehicles?${params.toString()}`
   );
 
-  return res.json() as Promise<Vehicle[]>;
+  const vehicles = res.json() as Promise<Vehicle[]>;
+
+  if (!vehicles) notFound();
+
+  return vehicles;
 };
 
 export const fetchFilters = async () => {
   const res = await fetch("http://localhost:3000/api/filters");
 
   return res.json() as Promise<Filter[]>;
+};
+
+export const fetchVehicle = async (id: string) => {
+  const res = await fetch(`http://localhost:3000/api/vehicles/${id}`);
+  const vehicle: Vehicle = await res.json();
+
+  if (!vehicle) notFound();
+
+  return vehicle;
 };
